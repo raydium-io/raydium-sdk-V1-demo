9b1a693d6f9b07a28e2cb5c948c206e6e7c9d04a
Jack <jack77121@gmail.com>
2023-02-03 16:49:13 +0800

    add assertion

--- src/swapOnlyAmm.ts
+++ src/swapOnlyAmm.ts
@@ -1,3 +1,5 @@
+import assert from 'assert';
+
 import {
   buildTransaction,
   jsonInfo2PoolKeys,
@@ -10,15 +12,8 @@ import {
 } from '@raydium-io/raydium-sdk';
 import { PublicKey } from '@solana/web3.js';
 
-import {
-  connection,
-  wallet,
-  wantBuildTxVersion,
-} from '../config';
-import {
-  getWalletTokenAccount,
-  sendTx,
-} from './util';
+import { connection, wallet, wantBuildTxVersion } from '../config';
+import { getWalletTokenAccount, sendTx } from './util';
 
 async function swapOnlyAmm() {
   // target pool public key string, in this example, USDC-RAY pool
@@ -26,9 +21,13 @@ async function swapOnlyAmm() {
   // get v2 pool list
   const ammV2Pool = await (await fetch('https://api.raydium.io/v2/sdk/liquidity/mainnet.json')).json(); // If the Liquidity pool is not required for routing, then this variable can be configured as undefined
   // get target pool
-  const targetPoolInfo = [...ammV2Pool.official, ...ammV2Pool.unOfficial].filter(
+  const targetPoolInfos = [...ammV2Pool.official, ...ammV2Pool.unOfficial].filter(
     (info) => info.id === targetPoolPublicKeyString
-  )[0];
+  );
+
+  assert(targetPoolInfos.length > 0, 'cannot find the target pool');
+
+  const targetPoolInfo = targetPoolInfos[0];
 
   // coin info
   const RAYToken = new Token(new PublicKey('4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R'), 6, 'RAY', 'RAY');
