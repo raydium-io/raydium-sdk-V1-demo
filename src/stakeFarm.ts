9ec3fea80336a0ec387e228216c8445b663b2b46
edsolater <edsolater@qq.com>
2023-02-10 23:50:04 +0800

    🚧 `stakeFarm()`

--- src/stakeFarm.ts
+++ src/stakeFarm.ts
@@ -1,5 +1,3 @@
-import assert from 'assert';
-
 import {
   ApiFarmInfo,
   buildTransaction,
@@ -7,83 +5,80 @@ import {
   FarmPoolKeys,
   jsonInfo2PoolKeys,
   Token,
-  TokenAmount,
-} from '@raydium-io/raydium-sdk';
-import { PublicKey } from '@solana/web3.js';
+  TokenAmount
+} from '@raydium-io/raydium-sdk'
+import { Keypair, PublicKey } from '@solana/web3.js'
+import assert from 'assert'
+import { connection, ENDPOINT, RAYDIUM_MAINNET_API, wallet, wantBuildTxVersion } from '../config'
+import { getWalletTokenAccount, sendTx } from './util'
 
-import {
-  connection,
-  ENDPOINT,
-  RAYDIUM_MAINNET_API,
-  wallet,
-  wantBuildTxVersion,
-} from '../config';
-import {
-  getWalletTokenAccount,
-  sendTx,
-} from './util';
+type WalletTokenAccounts = Awaited<ReturnType<typeof getWalletTokenAccount>>
+type TestTxInputInfo = {
+  targetFarm: string
+  inputTokenAmount: TokenAmount
+  walletTokenAccounts: WalletTokenAccounts
+  wallet: Keypair
+}
 
-async function stakeFarm() {
-  // target farm public key string, in this example, RAY-USDC farm
-  const targetFarmPublicKeyString = 'CHYrUBX2RKX8iBg7gYTkccoGNBzP44LdaazMHCLcdEgS';
-
-  // get farm pool list
-  const farmPool: ApiFarmInfo = await (await fetch(ENDPOINT + RAYDIUM_MAINNET_API.farmInfo)).json();
-  assert(farmPool, 'farm pool is undefined');
-
-  // get target farm json info
-  const targetFarmJsonInfo: any = farmPool.raydium.find((pool) => pool.id === targetFarmPublicKeyString);
-  assert(targetFarmJsonInfo, 'target farm not found');
-
-  // parse farm pool json info to to fit FarmPoolKeys type
-  const targetFarmInfo = jsonInfo2PoolKeys(targetFarmJsonInfo) as FarmPoolKeys;
-
-  // fetch target farm info
-  const farmFetchInfo = (await Farm.fetchMultipleInfoAndUpdate({
+/**
+ * pre-action: fetch target farm json info
+ *
+ * step 1: create instructions by SDK function
+ * step 2: compose instructions to several transactions
+ * step 3: send transactions
+ */
+async function stakeFarm(input: TestTxInputInfo) {
+  // -------- pre-action: fetch target farm json info --------
+  const farmPools: ApiFarmInfo = await (await fetch(ENDPOINT + RAYDIUM_MAINNET_API.farmInfo)).json()
+  assert(farmPools, 'farm pool is undefined')
+  const targetFarmJsonInfo: any = farmPools.raydium.find((pool) => pool.id === input.targetFarm)
+  assert(targetFarmJsonInfo, 'target farm not found')
+  const targetFarmInfo = jsonInfo2PoolKeys(targetFarmJsonInfo) as FarmPoolKeys
+  const { [input.targetFarm]: farmFetchInfo } = await Farm.fetchMultipleInfoAndUpdate({
     connection,
     pools: [targetFarmInfo],
-  }))[targetFarmPublicKeyString];
-  assert(
-    Object.keys(farmFetchInfo).length !== 0 && farmFetchInfo,
-    'cannot fetch target farm info'
-  );
+  })
+  assert(Object.keys(farmFetchInfo).length && farmFetchInfo, 'cannot fetch target farm info')
 
-  // get wallet token accounts
-  const walletTokenAccountFormat = await getWalletTokenAccount(connection, wallet.publicKey);
-
-  // prepare deposit amount
-  const lpToken = new Token(
-    new PublicKey('FbC6K13MzHvN42bXrtGaWsvZY9fxrackRSZcBGfjPc7m'),
-    6,
-    'RAY-USDC',
-    'RAY-USDC'
-  );
-  const inputTokenAmount = new TokenAmount(lpToken, 100);
-
-  // prepare instruction
+  // -------- step 1: create instructions by SDK function --------
   const makeDepositInstruction = await Farm.makeDepositInstructionSimple({
     connection,
     poolKeys: targetFarmInfo,
     fetchPoolInfo: farmFetchInfo,
     ownerInfo: {
-      feePayer: wallet.publicKey,
-      wallet: wallet.publicKey,
-      tokenAccounts: walletTokenAccountFormat,
+      feePayer: input.wallet.publicKey,
+      wallet: input.wallet.publicKey,
+      tokenAccounts: input.walletTokenAccounts,
     },
-    amount: inputTokenAmount.raw,
-  });
+    amount: input.inputTokenAmount.raw,
+  })
 
-  // prepare transactions
+  // -------- step 2: compose instructions to several transactions --------
   const makeDepositTransactions = await buildTransaction({
     connection,
     txType: wantBuildTxVersion,
-    payer: wallet.publicKey,
+    payer: input.wallet.publicKey,
     innerTransactions: makeDepositInstruction.innerTransactions,
-  });
+  })
 
-  // send transactions
-  const txids = await sendTx(connection, wallet, wantBuildTxVersion, makeDepositTransactions);
-  console.log(txids);
+  // -------- step 3: send transactions --------
+  const txids = await sendTx(connection, input.wallet, wantBuildTxVersion, makeDepositTransactions)
+  return { txids }
 }
 
-stakeFarm();
+async function howToUse() {
+  const targetFarm = 'CHYrUBX2RKX8iBg7gYTkccoGNBzP44LdaazMHCLcdEgS' // RAY-USDC farm
+  const lpToken = new Token(new PublicKey('FbC6K13MzHvN42bXrtGaWsvZY9fxrackRSZcBGfjPc7m'), 6, 'RAY-USDC', 'RAY-USDC')
+  const inputTokenAmount = new TokenAmount(lpToken, 100)
+  const walletTokenAccounts = await getWalletTokenAccount(connection, wallet.publicKey)
+
+  stakeFarm({
+    targetFarm,
+    inputTokenAmount,
+    walletTokenAccounts,
+    wallet: wallet,
+  }).then(({ txids }) => {
+    /** continue with txids */
+    console.log('txids', txids)
+  })
+}
