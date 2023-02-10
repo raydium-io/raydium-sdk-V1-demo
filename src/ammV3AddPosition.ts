89e627e1e634c76425279225b2e10d0747b035ca
edsolater <edsolater@qq.com>
2023-02-10 22:25:18 +0800

    🚧 `ammV3AddPosition()`

--- src/ammV3AddPosition.ts
+++ src/ammV3AddPosition.ts
@@ -1,5 +1,4 @@
-import assert from 'assert';
-
+import assert from 'assert'
 import {
   AmmV3,
   AmmV3PoolInfo,
@@ -7,101 +6,116 @@ import {
   ApiAmmV3PoolsItem,
   buildTransaction,
   ENDPOINT,
+  Percent,
   Token,
   TokenAmount,
-} from '@raydium-io/raydium-sdk';
-import { PublicKey } from '@solana/web3.js';
+} from '@raydium-io/raydium-sdk'
+import { Keypair, PublicKey } from '@solana/web3.js'
+import { connection, RAYDIUM_MAINNET_API, wallet, wantBuildTxVersion } from '../config'
+import { getWalletTokenAccount, sendTx } from './util'
 
-import {
-  connection,
-  RAYDIUM_MAINNET_API,
-  wallet,
-  wantBuildTxVersion,
-} from '../config';
-import {
-  getWalletTokenAccount,
-  sendTx,
-} from './util';
+type WalletTokenAccounts = Awaited<ReturnType<typeof getWalletTokenAccount>>
+type TestTxInputInfo = {
+  baseToken: Token
+  quoteToken: Token
+  targetPool: string
+  inputTokenAmount: TokenAmount
+  slippage: Percent
+  walletTokenAccounts: WalletTokenAccounts
+  wallet: Keypair
+  userCursorSide: 'base' | 'quote'
+}
 
-async function ammV3AddPosition() {
-  // target pool id, in this example, USDC-RAY pool
-  const targetPoolId = '61R1ndXxvsWXXkWSyNkCxnzwd3zUNB8Q2ibmkiLPC8ht';
-  // wallet accounts
-  const walletTokenAccountFormat = await getWalletTokenAccount(connection, wallet.publicKey);
-  // get all pool info from api
-  const ammV3Pool = (await (await fetch(ENDPOINT + RAYDIUM_MAINNET_API.ammV3Pools)).json()).data.filter(
-    (pool: ApiAmmV3PoolsItem) => pool.id === targetPoolId
-  );
+/**
+ * pre-action: fetch basic AmmV3 info
+ *
+ * step 1: ammV3 info and ammV3 position
+ * step 2: calculate liquidity
+ * step 3: create instructions by SDK function
+ * step 4: compose instructions to several transactions
+ * step 5: send transactions
+ */
+async function ammV3AddPosition(input: TestTxInputInfo): Promise<{ txids: string[] }> {
+  // -------- pre-action: fetch basic info --------
+  const ammV3Pools = (await fetch(ENDPOINT + RAYDIUM_MAINNET_API.ammV3Pools).then((res) => res.json())).data
+  const ammV3Pool = ammV3Pools.find((pool: ApiAmmV3PoolsItem) => pool.id === input.targetPool)
 
-  let ammV3PoolInfoList: AmmV3PoolInfo[] = [];
-  let ammV3PersonalPositionList: AmmV3PoolPersonalPosition[] = [];
-  Object.values(
-    await AmmV3.fetchMultiplePoolInfos({
-      connection,
-      poolKeys: ammV3Pool,
-      chainTime: new Date().getTime() / 1000,
-      ownerInfo: {
-        wallet: wallet.publicKey,
-        tokenAccounts: walletTokenAccountFormat,
-      },
-    })
-  ).forEach((i) => {
-    ammV3PoolInfoList.push(i.state);
-    if (i.positionAccount) {
-      ammV3PersonalPositionList = i.positionAccount;
-    }
-  });
+  // -------- step 1: ammV3 info and ammV3 position --------
+  const { [ammV3Pool.id]: sdkParsedAmmV3Info } = await AmmV3.fetchMultiplePoolInfos({
+    connection,
+    poolKeys: [ammV3Pool],
+    chainTime: new Date().getTime() / 1000,
+    ownerInfo: {
+      wallet: wallet.publicKey,
+      tokenAccounts: input.walletTokenAccounts,
+    },
+  })
+  const { state: ammV3PoolInfo, positionAccount } = sdkParsedAmmV3Info
+  assert(positionAccount && positionAccount.length, "position is not exist/is empty, so can't continue to add position")
+  const ammV3Position = positionAccount[0] // assume first one is your target
 
-  // if no pool info, abort
-  assert(ammV3PoolInfoList.length > 0, 'cannot find the target pool info');
-  // if no position
-  assert(ammV3PersonalPositionList.length > 0, 'no position in the target pool');
-
-  // get the first pool info (which is our target pool)
-  const ammV3PoolInfo = ammV3PoolInfoList[0];
-  // get the first position we have (you can choose other position for adding)
-  const ammV3Position = ammV3PersonalPositionList[0];
-
-  // prepare base token amount
-  const RAYToken = new Token(new PublicKey('4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R'), 6, 'RAY', 'RAY');
-  const inputTokenAmount = new TokenAmount(RAYToken, 10000);
-
-  // calculate liquidity base on 'base token amount'
+  // -------- step 2: calculate liquidity --------
+  const isFocus1 = input.userCursorSide === 'base'
+  const isCoin1Base = input.baseToken.mint.equals(ammV3Pool.state.mintA.mint)
+  const isPairPoolDirectionEq = (isFocus1 && isCoin1Base) || (!isCoin1Base && !isFocus1)
   const { liquidity } = AmmV3.getLiquidityAmountOutFromAmountIn({
     poolInfo: ammV3PoolInfo,
     slippage: 0,
-    inputA: true,
+    inputA: isPairPoolDirectionEq,
     tickUpper: ammV3Position.tickUpper,
     tickLower: ammV3Position.tickLower,
-    amount: inputTokenAmount.raw,
+    amount: input.inputTokenAmount.raw,
     add: true, // SDK flag for math round direction
-  });
+  })
 
-  // prepare instruction
+  // -------- step 3: create instructions by SDK function --------
   const makeIncreaseLiquidityInstruction = await AmmV3.makeIncreaseLiquidityInstructionSimple({
     connection,
     poolInfo: ammV3PoolInfo,
     ownerPosition: ammV3Position,
     ownerInfo: {
-      feePayer: wallet.publicKey,
-      wallet: wallet.publicKey,
-      tokenAccounts: walletTokenAccountFormat,
+      feePayer: input.wallet.publicKey,
+      wallet: input.wallet.publicKey,
+      tokenAccounts: input.walletTokenAccounts,
     },
     liquidity,
     slippage: 1,
-  });
+  })
 
-  // prepare transactions
+  // -------- step 4: compose instructions to several transactions --------
   const makeIncreaseLiquidityTransactions = await buildTransaction({
     connection,
     txType: wantBuildTxVersion,
-    payer: wallet.publicKey,
+    payer: input.wallet.publicKey,
     innerTransactions: makeIncreaseLiquidityInstruction.innerTransactions,
-  });
+  })
 
-  // send transactions
-  const txids = await sendTx(connection, wallet, wantBuildTxVersion, makeIncreaseLiquidityTransactions);
-  console.log(txids);
+  // -------- step 5: send transactions --------
+  const txids = await sendTx(connection, input.wallet, wantBuildTxVersion, makeIncreaseLiquidityTransactions)
+  return { txids }
 }
 
-ammV3AddPosition();
+async function howToUse() {
+  const baseToken = new Token(new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'), 6, 'USDC', 'USDC') // USDC
+  const quoteToken = new Token(new PublicKey('4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R'), 6, 'RAY', 'RAY') // RAY
+  const targetPool = '61R1ndXxvsWXXkWSyNkCxnzwd3zUNB8Q2ibmkiLPC8ht' // USDC-RAY pool
+  const RAYToken = new Token(new PublicKey('4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R'), 6, 'RAY', 'RAY')
+  const inputTokenAmount = new TokenAmount(RAYToken, 10000)
+  const slippage = new Percent(1, 100)
+  const walletTokenAccounts = await getWalletTokenAccount(connection, wallet.publicKey)
+  const userCursorSide: 'base' | 'quote' = 'base'
+
+  ammV3AddPosition({
+    baseToken,
+    quoteToken,
+    targetPool,
+    inputTokenAmount,
+    slippage,
+    walletTokenAccounts,
+    wallet: wallet,
+    userCursorSide,
+  }).then(({ txids }) => {
+    /** continue with txids */
+    console.log('txids', txids)
+  })
+}
