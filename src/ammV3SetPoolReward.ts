b34f492d7be8ac83a0f15c8f24c2bcceb47d04a0
edsolater <edsolater@qq.com>
2023-02-10 23:23:21 +0800

    🚧 `ammV3SetPoolReward()`

--- src/ammV3SetPoolReward.ts
+++ src/ammV3SetPoolReward.ts
@@ -1,52 +1,52 @@
-import Decimal from 'decimal.js';
+import Decimal from 'decimal.js'
 
-import {
-  AmmV3,
-  ApiAmmV3PoolsItem,
-  buildTransaction,
-  ENDPOINT,
-} from '@raydium-io/raydium-sdk';
-import { PublicKey } from '@solana/web3.js';
+import { AmmV3, ApiAmmV3PoolsItem, buildTransaction, ENDPOINT, Token } from '@raydium-io/raydium-sdk'
+import { Keypair, PublicKey } from '@solana/web3.js'
 
-import {
-  connection,
-  RAYDIUM_MAINNET_API,
-  wallet,
-  wantBuildTxVersion,
-} from '../config';
-import {
-  getWalletTokenAccount,
-  sendTx,
-} from './util';
+import { connection, RAYDIUM_MAINNET_API, wallet, wantBuildTxVersion } from '../config'
+import { getWalletTokenAccount, sendTx } from './util'
 
-// THIS DEMO HAS NOT BEEN TESTING YET!!!!!
+type WalletTokenAccounts = Awaited<ReturnType<typeof getWalletTokenAccount>>
+type TestTxInputInfo = {
+  targetPool: string
+  walletTokenAccounts: WalletTokenAccounts
+  wallet: Keypair
+  rewardInfos: { token: Token; openTime: number; endTime: number; perSecond: Decimal }[]
+}
 
-async function ammV3SetPoolReward() {
-  // target pool id, in this example, USDC-RAY pool
-  const targetPoolId = '61R1ndXxvsWXXkWSyNkCxnzwd3zUNB8Q2ibmkiLPC8ht';
-  // get all pool info from api
-  const ammV3Pool = (await (await fetch(ENDPOINT + RAYDIUM_MAINNET_API.ammV3Pools)).json()).data.filter(
-    (pool: ApiAmmV3PoolsItem) => pool.id === targetPoolId
-  );
+/**
+ * pre-action: fetch basic AmmV3 info
+ *
+ * step 1: ammV3 info
+ * step 2: create set reward instructions
+ * step 3: compose instructions to several transactions
+ * step 4: send transactions
+ */
+async function ammV3SetPoolReward(input: TestTxInputInfo) {
+  // -------- pre-action: fetch basic info --------
+  const ammV3Pools = (await fetch(ENDPOINT + RAYDIUM_MAINNET_API.ammV3Pools).then((res) => res.json())).data
+  const ammV3Pool = ammV3Pools.find((pool: ApiAmmV3PoolsItem) => pool.id === input.targetPool)
 
-  // get the first pool info
-  const ammV3PoolInfo = (await AmmV3.fetchMultiplePoolInfos({
+  // -------- step 1: ammV3 info  --------
+  const { [ammV3Pool.id]: sdkParsedAmmV3Info } = await AmmV3.fetchMultiplePoolInfos({
     connection,
-    poolKeys: ammV3Pool,
+    poolKeys: [ammV3Pool],
     chainTime: new Date().getTime() / 1000,
-  }))[targetPoolId]
+    ownerInfo: {
+      wallet: input.wallet.publicKey,
+      tokenAccounts: input.walletTokenAccounts,
+    },
+  })
+  const { state: ammV3PoolInfo } = sdkParsedAmmV3Info
 
-  // get wallet token accounts
-  const walletTokenAccountFormat = await getWalletTokenAccount(connection, wallet.publicKey);
-
-  // prepare instruction
+  // -------- step 2: create set reward instructions --------
   const makeSetRewardsInstruction = await AmmV3.makeSetRewardsInstructionSimple({
     connection,
-    poolInfo: ammV3PoolInfo.state,
+    poolInfo: ammV3PoolInfo,
     ownerInfo: {
-      feePayer: wallet.publicKey,
-      wallet: wallet.publicKey,
-      tokenAccounts: walletTokenAccountFormat,
+      feePayer: input.wallet.publicKey,
+      wallet: input.wallet.publicKey,
+      tokenAccounts: input.walletTokenAccounts,
     },
     rewardInfos: [
       {
@@ -57,19 +57,41 @@ async function ammV3SetPoolReward() {
       },
     ],
     chainTime: new Date().getTime() / 1000,
-  });
+  })
 
-  // prepare transactions
+  // -------- step 3: compose instructions to several transactions --------
   const openPoolPositiontransactions = await buildTransaction({
     connection,
     txType: wantBuildTxVersion,
     payer: wallet.publicKey,
     innerTransactions: makeSetRewardsInstruction.innerTransactions,
-  });
+  })
 
-  // send transactions
-  const txids = await sendTx(connection, wallet, wantBuildTxVersion, openPoolPositiontransactions);
-  console.log(txids);
+  // -------- step 4: send transactions --------
+  const txids = await sendTx(connection, wallet, wantBuildTxVersion, openPoolPositiontransactions)
+  return { txids }
 }
 
-ammV3SetPoolReward();
+async function howToUse() {
+  const targetPool = '61R1ndXxvsWXXkWSyNkCxnzwd3zUNB8Q2ibmkiLPC8ht' // USDC-RAY pool
+  const RAYToken = new Token(new PublicKey('4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R'), 6, 'RAY', 'RAY')
+  const walletTokenAccounts = await getWalletTokenAccount(connection, wallet.publicKey)
+  const rewardInfos = [
+    {
+      token: RAYToken,
+      openTime: 4073858467, // Wed Feb 04 2099 03:21:07 GMT+0000
+      endTime: 4076277667, // Wed Mar 04 2099 03:21:07 GMT+0000
+      perSecond: new Decimal(0.000001),
+    },
+  ]
+
+  ammV3SetPoolReward({
+    targetPool,
+    walletTokenAccounts,
+    wallet: wallet,
+    rewardInfos,
+  }).then(({ txids }) => {
+    /** continue with txids */
+    console.log('txids', txids)
+  })
+}
